89d86089a4054c5ab5ee3256b09a16fd17d69738
rishi16-19 <rishimishra1619@gmail.com>
2025-05-09 02:05:07 +0530

    Added initial backend

---
+++ Backend/Resume_Analyzer/src/test/java/com/rishi/resume_analyzer/ResumeAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rishi.resume_analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
